a5a24210c238b43a5ab0c31a42fcd52226d134c3
Jose Perez <j.perez@adinspector.pe>
2016-10-15 07:53:24 -0500

    Initial project

---
+++ app/js/script.js
@@ -0,0 +1,326 @@
+'use strict';
+
+var j = jQuery.noConflict();
+
+(function($){
+  var $win = j(window);
+  var $doc = j(document);
+  var $animationElements = j('.animation-element');
+  var $sliders = j('.Sliders');
+  var $formSubs = j('.Course-subs');
+  var $sidebar = j('.Sidebar');
+  var body = j('body');
+  var $header = j('.header');
+  var $footer = j('.Footer');
+
+  function affixHeader() {
+    if ($sliders.length) {
+      j('#affixHeader').affix({
+        offset: {
+          top: function() {
+            return ($sliders.outerHeight(true) / 2) - 80;
+          }
+        }
+      });
+    }
+  }
+
+  function affixMenuCepuch() {
+    var cepuch = j('#js-cepuch');
+
+    if (cepuch.length) {
+      j('#affixMenuCepuch').affix({
+        offset: {
+          top: function() {
+            return (cepuch.outerHeight(true) - 80);
+          }
+        }
+      });
+    }
+  }
+
+  function affixFormSubs() {
+    if ($formSubs.length) {
+      j('#affixFormSubs').affix({
+        offset: {
+          top: function() {
+            return $header.outerHeight(true) + 40;
+          },
+          bottom: function() {
+            return (this.bottom = $footer.outerHeight(true) + 100);
+          }
+        }
+      });
+    }
+  }
+
+  function checkPositionFormSubs() {
+    if ($formSubs.length) {
+      j('.Course-tabs li a[data-toggle="tab"]').on('show.bs.tab', function(e){
+        j('#affixFormSubs').removeClass('bug');
+      });
+
+      j('#affixFormSubs').on('affix-bottom.bs.affix', function(e){
+        if (j('.Course-tabs-content').height() < 500) {
+          j(e.target).addClass('bug');
+        } else {
+          j(e.target).removeClass('bug');
+        }
+      });
+    }
+  }
+
+  function affixSidebar() {
+    if ($sidebar.length) {
+      j('#affixSidebar').affix({
+        offset: {
+          top: function() {
+            return $header.outerHeight(true) + 40;
+          },
+          bottom: function() {
+            return (this.bottom = $footer.outerHeight(true) + 120);
+          }
+        }
+      });
+    }
+  }
+
+  function scrollHeader() {
+    var logoNormal = j('#js-logo-normal'),
+      logoScroll = j('#js-logo-scroll');
+
+    if ($sliders.length) {
+      var limit = ($sliders.outerHeight(true) / 2) - 80;
+
+      if ($win.scrollTop() > limit) {
+        logoNormal.removeClass('active');
+        logoScroll.addClass('active');
+      } else {
+        logoNormal.addClass('active');
+        logoScroll.removeClass('active');
+      }
+    }
+  }
+
+  function loaderEffect() {
+    j("#loader").delay(500).fadeOut();
+    j(".LoaderWrapper").delay(1000).fadeOut("slow");
+  }
+
+  function changeLogoHeader() {
+    if (!$sliders.length) {
+      var logoNormal = j('#js-logo-normal'),
+        logoScroll = j('#js-logo-scroll');
+
+      logoNormal.removeClass('active');
+      logoScroll.addClass('active');
+    }
+  }
+
+  function loadingProcess() {
+    var loadedimages = 0;
+    var loadedpercent = 0;
+
+    body.imagesLoaded()
+    .always( function( instance ) {
+      //console.log('loading images');
+    })
+    .fail( function() {
+      //console.log('all images loaded, at least one is broken');
+    })
+    .done( function( instance ) {
+
+    })
+    .progress( function( instance, image ) {
+      var totalimage = instance.images.length;
+      var result = image.isLoaded ? 'loaded' : 'broken';
+      if(result==='loaded'){
+        loadedimages++;
+      }
+      loadedpercent = Math.round((loadedimages/totalimage)*100);
+      jQuery('#loadingbar').css('width', loadedpercent+'%');
+      //console.log( 'image is ' + result + ' for ' + image.img.src );
+    });
+  }
+
+  function initTestimonials() {
+    if (j('.Carousel-multiple').length) {
+      j('.Carousel-multiple .item').each(function(){
+        var itemToClone = j(this);
+
+        for (var i=1;i<3;i++) {
+          itemToClone = itemToClone.next();
+
+          // wrap around if at end of item collection
+          if (!itemToClone.length) {
+            itemToClone = $(this).siblings(':first');
+          }
+
+          // grab item, clone, add marker class, add to collection
+          itemToClone.children(':first-child').clone()
+            .addClass("cloneditem-"+(i))
+            .appendTo(j(this));
+        }
+      });
+    }
+  }
+
+  $win.on('load', function(){
+    loaderEffect();
+
+    changeLogoHeader();
+  });
+
+  $win.on('scroll', function(event){
+    if(j(this).scrollTop() > 150) {
+      j('.ArrowTop').fadeIn();
+    } else {
+      j('.ArrowTop').fadeOut();
+    }
+
+    if ($win.width() > 991) {
+      scrollHeader();
+    }
+  });
+
+  $win.on('resize', function(){
+    affixHeader();
+    affixFormSubs();
+    checkPositionFormSubs();
+    affixSidebar();
+    affixMenuCepuch();
+  });
+
+  j(document).on('ready', function(){
+    loadingProcess();
+
+    initTestimonials();
+
+    affixHeader();
+    affixFormSubs();
+    checkPositionFormSubs();
+    affixSidebar();
+    affixMenuCepuch();
+
+    j('.js-move-scroll').on('click', function(event) {
+      event.preventDefault();
+      var $this = j(this);
+      var dest = $this.attr('href');
+
+      dest = (dest.charAt(0) === '#') ? dest : '#' + dest;
+
+      j('html, body').stop().animate({
+        scrollTop: j(dest).offset().top
+      }, 2000, 'easeInOutExpo');
+    });
+
+    j('.ArrowTop').on('click', function(ev){
+      ev.preventDefault();
+      j('html, body').animate({scrollTop: 0}, 800);
+    });
+
+    j('.Header-MainMenu-list-item a').on('click', function(e){
+      var $this = j(this),
+        redirect = Boolean($this.data('redirect')),
+        $target = j(this.hash);
+
+      if (!redirect) {
+        e.preventDefault();
+
+        j('html, body').stop().animate({
+          'scrollTop': $target.offset().top + 2
+        }, 2000, 'easeInOutExpo');
+      } else {
+        $this.submit();
+      }
+    });
+
+    j('.Card a').on('click', function(e){
+      var menu = j('#affixMenuCepuch');
+
+      var $this = j(this),
+        $target = j(this.hash);
+
+      e.preventDefault();
+
+      if (menu.hasClass('affix-top')) {
+        return false;
+      } else {
+        j('html, body').stop().animate({
+          'scrollTop': $target.offset().top + 2
+        }, 2000, 'easeInOutExpo');
+      }
+    });
+
+    body.on('click', '.Submenu-more', function(ev){
+      ev.preventDefault();
+      var $this = j(this),
+        parent = Boolean($this.data('parent')),
+        status = Boolean($this.data('status'));
+
+      if (parent) {
+        j('.Submenu-more').each(function(index) {
+          var $t = j(this);
+          $t.next().hide();
+          $t.removeClass('minus').addClass('plus').data('status', false);
+        });
+
+      } else {
+        $this.parent().parent().find('.sub-menu').hide();
+        $this.parent().parent().find('.Submenu-more').removeClass('minus').addClass('plus').data('status', false);
+      }
+
+      if (!status) {
+        $this.next().show();
+        $this.removeClass('plus').addClass('minus');
+      } else {
+        $this.next().hide();
+        $this.removeClass('minus').addClass('plus');
+      }
+
+      status = !status;
+      $this.data('status', status);
+    });
+
+    // Mobile Slidebars
+    j.slidebars();
+
+    // Swipe carousel bootstrap
+    j(".carousel").swipe({
+      swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+        if (direction == 'left') $(this).carousel('next');
+        if (direction == 'right') $(this).carousel('prev');
+      },
+      allowPageScroll:"vertical"
+    });
+
+    // j('.Course-tabs li a[data-toggle="tab"]').on('show.bs.tab', function(e){
+    //   // e.target;
+    //   // e.relatedTarget;
+    //   checkAffixSubs();
+    // });
+  });
+
+  function checkIfInView() {
+    var windowHeight = $win.height();
+    var windowTopPosition = $win.scrollTop();
+    var windowBottomPosition = (windowTopPosition + windowHeight);
+
+    j.each($animationElements, function(){
+      var element = j(this);
+      var animation = element.data('animation');
+      var elementHeight = element.outerHeight();
+      var elementTopPosition = element.offset().top;
+      var elementBottomPosition = (elementTopPosition + elementHeight);
+
+      if ((elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition)) {
+        element.addClass(animation);
+      } else {
+        element.removeClass(animation);
+      }
+    });
+  }
+
+  $win.on('scroll resize', checkIfInView);
+  $win.trigger('scroll');
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,181 @@
+'use strict'
+
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    compass = require('gulp-compass'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    inject = require('gulp-inject'),
+    wiredep = require('wiredep').stream,
+    gulpif = require('gulp-if'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    uncss = require('gulp-uncss'),
+    clean = require('gulp-clean')
+
+var paths = {
+  compass: ['./app/sass/**/*.sass']
+}
+
+// Pre-procesa archivos Sass a CSS y recarga los cambios
+gulp.task('compass', function(){
+  gulp.src(paths.compass)
+    .pipe(compass({
+      css: './app/css/',
+      sass: './app/sass/',
+      image: './app/images/',
+      font: './app/fonts/'
+    }))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(livereload())
+})
+
+// Recarga el navegador cuando hay cambios en el HTML
+gulp.task('html', function(){
+  gulp.src(['./app/*.html'])
+    .pipe(livereload())
+})
+
+// Recarga el navegador cuando hay cambios en el PHP de wp
+gulp.task('phpwp', function(){
+  // gulp.src(['./public/**/*.html', './app/**/*.html'])
+  gulp.src(['./wp-content/themes/cepuch/**/*.php'])
+    .pipe(livereload())
+})
+
+// Busca errores en el JS y nos los muestra por pantalla
+gulp.task('jshint', function(){
+  return gulp.src('./app/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+})
+
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el default.html
+gulp.task('inject', function(){
+  var target = gulp.src('./app/*.html')
+
+  return target.pipe(inject(gulp.src('./app/css/**/*.css', {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./app/js/**/*.js', {read: false}), {relative: true}))
+    .pipe(gulp.dest('./app'))
+})
+
+// Inyecta las librerias que instalamos vía Bower
+gulp.task('wiredep', function(){
+  gulp.src('./app/**/*.html')
+    .pipe(wiredep({
+      directory: './app/lib'
+    }))
+    .pipe(gulp.dest('./app'))
+})
+
+// Comprime los archivos CSS y JS enlazados en el default.html
+//  y los minifica
+gulp.task('compress', function()  {
+  //gulp.src('./app/**/*.html')
+  gulp.src('./app/*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js',  uglify()))
+    // .pipe(gulpif('*.js',  uglify({mangle: false })))
+    .pipe(gulpif('*.css', cssnano()))
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('minifycss', function(){
+  return gulp.src('./public/css/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('./wp-content/themes/cepuch/css'))
+})
+
+gulp.task('minifyjs', function(){
+  return gulp.src('./public/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./wp-content/themes/cepuch/js'))
+})
+
+// Elimina el CSS que no es utilizado para reducir el peso del archivo
+gulp.task('uncss', ['compress'], function(){
+  gulp.src('./public/css/style.min.css')
+    .pipe(uncss({
+      html: [
+        './public/index.html',
+      ]
+    }))
+    .pipe(gulp.dest('./public/css'))
+})
+
+// Copia el contenido de los estáticos e default.html al directorio
+// de producción sin tags de comentarios
+gulp.task('copy', ['uncss'], function(){
+  //gulp.src('./app/**/*.html')
+  gulp.src('./app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('./public'))
+  gulp.src('./app/images/**')
+    .pipe(gulp.dest('./public/images'))
+  gulp.src('./app/fonts/**')
+    .pipe(gulp.dest('./public/fonts'))
+  gulp.src('./app/lib/bootstrap/fonts/**')
+    .pipe(gulp.dest('./public/fonts'))
+})
+
+// Copia el contenido de los estáticos al directorio
+// de producción de wp sin tags de comentarios
+gulp.task('copywp', function(){
+  gulp.src('./public/images/**')
+    .pipe(gulp.dest('./wp-content/themes/cepuch/images'))
+  gulp.src('./public/fonts/**')
+    .pipe(gulp.dest('./wp-content/themes/cepuch/fonts'))
+  gulp.src('./public/videos/**')
+    .pipe(gulp.dest('./wp-content/themes/cepuch/videos'))
+})
+
+gulp.task('watch', function(){
+  livereload.listen()
+  gulp.watch(['./app/**/*.html'], ['html'])
+  gulp.watch(paths.compass, ['compass', 'inject'])
+  gulp.watch(['./app/js/**/*.js'], ['jshint', 'inject'])
+  // gulp.watch(['./bower.json'], ['wiredep'])
+})
+
+gulp.task('watchwp', function(){
+  livereload.listen()
+  gulp.watch(['./wp-content/themes/cepuch/**/*.php'], ['phpwp'])
+})
+
+gulp.task('cleanpublic', function(){
+  return gulp.src([
+      'public/js',
+      'public/css',
+      'public/images/',
+      'public/fonts',
+      'public/*.html'
+    ], {read: false})
+    .pipe(clean())
+})
+
+gulp.task('cleanwp', function(){
+  return gulp.src([
+    'wp-content/themes/cepuch/js/app.min.js',
+    'wp-content/themes/cepuch/js/vendor.min.js',
+    'wp-content/themes/cepuch/images',
+    'wp-content/themes/cepuch/fonts',
+    'wp-content/themes/cepuch/css/style.min.css',
+    'public/*.html'
+    ], {read: false})
+    .pipe(clean())
+})
+
+gulp.task('default', ['inject', 'watch'])
+// gulp.task('default', ['inject', 'wiredep', 'watch'])
+
+gulp.task('dev', ['watch'])
+gulp.task('devwp', ['watchwp'])
+gulp.task('bower', ['wiredep'])
+
+gulp.task('build', ['copy'])
+gulp.task('buildwp', ['copywp', 'minifycss', 'minifyjs'])
+
+gulp.task('del', ['cleanpublic', 'cleanwp'])
